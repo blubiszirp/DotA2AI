e322aa55305570396fa35c40279f40dc1a99889c
Frank Jordi <frank-jordi@hotmail.com>
2016-12-19 19:18:24 +0100

    add StateMachine to Viper

--- bots/bot_viper.lua
+++ bots/bot_viper.lua
@@ -1,32 +1,43 @@
 require( GetScriptDirectory().."/locations/locations" )
+
 ----------------------------------------------------------------------------------------
-----------------------------------------------------------------------------------------
--- Enchantress Radiant Mid
-----------------------------------------------------------------------------------------
+-- Ability
 ----------------------------------------------------------------------------------------
 function ThinkLvlupAbility()
     local viper = GetBot();
 	--print("Try to LevelUp Ability");
-    enchantress:Action_LevelAbility("enchantress_untouchable");
+    --viper:Action_LevelAbility("enchantress_untouchable");
 end
 
+----------------------------------------------------------------------------------------
+-- Variables
+----------------------------------------------------------------------------------------
+local STATE_PREGAME_RADIANT = "STATE_PREGAME_RADIANT";
+local STATE_T1_FARMING_MID = "STATE_T1_FARMING_MID";
+local STATE_RUN_AWAY = "STATE_RUN_AWAY";
 
-function MoveToRadiantSecretShop()
+
+----------------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------
+-- Viper Radiant Mid
+----------------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------
+local function MoveToRadiantSecretShop()
 	local viper = GetBot();
 	viper:Action_MoveToLocation(RADIANT_SECRET_SHOP_POS);
 end
 
-function MoveToRadiantBountyRuneTop()
+local function MoveToRadiantBountyRuneTop()
 	local viper = GetBot();
-	viper:Action_MoveToLocation(RADIANT_BOUNTY_TOP_POS);
+	viper:Action_MoveToLocation(RADIANT_BOUNTY_RUNE_OFF_POS);
 end
 
-function MoveToRadiantMidT1()
+local function MoveToRadiantMidT1()
 	local viper = GetBot();
-	viper:Action_MoveToLocation(Vector(0,0,0));
+	viper:Action_MoveToLocation(Vector(-1400,-1300,0));
 end
 
-function PreGameRadiantThink()
+local function PreGameRadiant(StateMachine)
 	local viper = GetBot();
 	if DotaTime()<-10 then
 		MoveToRadiantSecretShop();
@@ -36,17 +47,25 @@ function PreGameRadiantThink()
 		viper:Action_PickUpRune(viper);
 	else
 		MoveToRadiantMidT1();
-	end
-end
-
-function RadiantThink()
-	if DotaTime()<=1 then
-		PreGameRadiantThink();
+		StateMachineState = STATE_T1_FARMING_MID;
 	end
 end
 
 ----------------------------------------------------------------------------------------
+-- State Machine
 ----------------------------------------------------------------------------------------
+local StateMachine = {};
+StateMachine["State"] = STATE_PREGAME_RADIANT;
+StateMachine[STATE_PREGAME_RADIANT] = PreGameRadiant;
+StateMachine[STATE_T1_FARMING_MID] = PreGameRadiant;
+
+----------------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------
+local function RadiantThink()
+	print("STATE: "..StateMachine.State);
+	StateMachine[StateMachine.State](StateMachine);
+end
+
 
 function Think()
 	if ( GetTeam() == TEAM_RADIANT )
